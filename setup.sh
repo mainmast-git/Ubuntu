9e516b4dddd74675f847886e110f4d9d4a539af4
Ramin Samadi <ramin-samadi@outlook.com>
2025-03-08 20:30:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,107 +2,122 @@
 
 echo "Starting Ubuntu Post-Installation Setup..."
 
-# Update & Upgrade System
-echo "Updating and upgrading the system..."
-sudo apt update -y; sudo apt upgrade -y
+# Function to update and upgrade system
+update_system() {
+    echo "Updating and upgrading the system..."
+    sudo apt update -y && sudo apt upgrade -y
+}
 
-# Add GNS3 PPA
-echo "Adding GNS3 PPA..."
-sudo add-apt-repository -y ppa:gns3/ppa
-sudo apt update -y
+# Function to add PPAs
+add_ppas() {
+    echo "Adding PPAs..."
+    sudo add-apt-repository -y ppa:gns3/ppa
+    sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
+    sudo add-apt-repository -y ppa:papirus/papirus
+    sudo apt update -y
+}
 
-# Add PPAs needed for fastfetch and papirus-theme
-echo "Adding fastfetch PPA..."
-sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
-sudo add-apt-repository ppa:papirus/papirus
-sudo apt update -y
+# Function to install APT packages
+install_apt_packages() {
+    echo "Installing required packages..."
+    sudo apt install -y vim curl git qemu-kvm libvirt-daemon-system libvirt-clients \
+        bridge-utils virt-manager flatpak timeshift neovim qdirstat qt5ct \
+        qt5-style-kvantum qt5-style-kvantum-themes gns3-gui gns3-server libminizip1 \
+        libxcb-xinerama0 tldr fastfetch lsd make gawk trash-cli fzf bash-completion \
+        whois bat tree ripgrep gnome-tweaks plocate fail2ban papirus-icon-theme \
+        epapirus-icon-theme
+}
 
-# Install APT Packages
-echo "Installing required packages..."
-sudo apt install -y \
-    vim curl git qemu-kvm libvirt-daemon-system libvirt-clients \
-    bridge-utils virt-manager flatpak timeshift neovim qdirstat \
-    qt5ct qt5-style-kvantum qt5-style-kvantum-themes gns3-gui \
-    gns3-server libminizip1 libxcb-xinerama0 tldr fastfetch lsd \
-    make gawk trash-cli fzf bash-completion whois bat tree \
-    ripgrep gnome-tweaks plocate fail2ban papirus-icon-theme \
-    epapirus-icon-theme
+# Function to setup QT5 theme
+setup_qt5_theme() {
+    echo "Setting up theme (Fusion + GTK3 + darker) for KDE..."
+    qt5ct
+    sudo qt5ct
+}
 
-# Setup qt5ct theme for KDE applications
-echo "Setting up theme (Fusion + GTK3 + darker) for KDE..."
-qt5ct # For user
-sudo qt5ct # For super user 
+# Function to add i386 architecture and install GNS3 IOU
+install_gns3_iou() {
+    echo "Adding i386 architecture and updating packages..."
+    sudo dpkg --add-architecture i386
+    sudo apt update -y
+    sudo apt install -y gns3-iou
+}
 
-# Enable i386 architecture for GNS3 IOU support
-echo "Adding i386 architecture and updating packages..."
-sudo dpkg --add-architecture i386
-sudo apt update -y
-sudo apt install -y gns3-iou
+# Function to install .deb packages
+install_deb_packages() {
+    echo "Downloading and installing Google Chrome & TeamViewer..."
+    wget -O /tmp/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    wget -O /tmp/teamviewer.deb https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+    sudo dpkg -i /tmp/google-chrome.deb /tmp/teamviewer.deb || sudo apt install -f -y
+}
 
-# Install .deb Packages
-echo "Downloading and installing Google Chrome & TeamViewer..."
-wget -O /tmp/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget -O /tmp/teamviewer.deb https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-sudo dpkg -i /tmp/google-chrome.deb /tmp/teamviewer.deb || sudo apt install -f -y
+# Function to setup Flatpak
+setup_flatpak() {
+    echo "Setting up Flatpak..."
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    echo "Installing Flatpak apps..."
+    flatpak install -y flathub com.rustdesk.RustDesk com.usebottles.bottles com.spotify.Client \
+        io.github.shiftey.Desktop io.missioncenter.MissionCenter com.obsproject.Studio \
+        com.obsproject.Studio.Plugin.DroidCam
+    flatpak install --user -y https://sober.vinegarhq.org/sober.flatpakref
+}
 
-# Install Flatpak and Flathub repository
-echo "Setting up Flatpak..."
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# Function to configure GNOME
+configure_gnome() {
+    echo "Configuring GNOME theme..."
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+    gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/]$/, 'google-chrome.desktop']/")"
+    gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/]$/, 'com.spotify.Client.desktop']/")"
+    gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/]$/, 'gns3.desktop']/")"
+    gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/, 'firefox_firefox.desktop'//" | sed "s/'firefox_firefox.desktop', //")"
+    gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/, 'libreoffice-writer.desktop'//" | sed "s/'libreoffice-writer.desktop', //")"
+    gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/, 'snap-store_snap-store.desktop'//" | sed "s/'snap-store_snap-store.desktop', //")"
+    gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/, 'thunderbird_thunderbird.desktop'//" | sed "s/'thunderbird_thunderbird.desktop', //")"
+    gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/, 'yelp.desktop'//" | sed "s/'yelp.desktop', //")"
+    gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/, 'org.gnome.Rhythmbox3.desktop'//" | sed "s/'org.gnome.Rhythmbox3.desktop', //")"
+}
 
-echo "Installing Flatpak apps..."
-flatpak install -y flathub \
-com.rustdesk.RustDesk com.usebottles.bottles com.spotify.Client \
-io.github.shiftey.Desktop io.missioncenter.MissionCenter \
-com.obsproject.Studio com.obsproject.Studio.Plugin.DroidCam
+# Function to remove unwanted apps
+remove_unwanted_apps() {
+    echo "Removing unwanted apps..."
+    sudo snap remove thunderbird firefox
+    sudo apt autoremove -y
+}
 
-flatpak install --user -y https://sober.vinegarhq.org/sober.flatpakref
+# Function to clone repositories
+clone_repositories() {
+    echo "Cloning configuration repository..."
+    git clone https://github.com/ramin-samadi/Ubuntu /tmp/Ubuntu
+    git clone https://github.com/orangci/walls-catppuccin-mocha.git ~/Wallpapers
+    sudo mv /tmp/Ubuntu/usr/local/bin/change_wallpaper.sh /usr/local/bin/
+}
 
-# Set Dark Mode in GNOME
-echo "Configuring GNOME theme..."
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+# Function to copy configuration files
+copy_config_files() {
+    echo "Deploying user configurations..."
+    sudo mv -f /tmp/Ubuntu/home/.config/* $HOME/.config/
+    sudo mv -f /tmp/Ubuntu/home/.vimrc $HOME/
+}
 
-# Pin favorite apps to the Ubuntu sidebar
-gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/]$/, 'google-chrome.desktop']/")"
-gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/]$/, 'com.spotify.Client.desktop']/")"
-gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/]$/, 'gns3.desktop']/")"
+# Function to enable firewall and Fail2Ban
+enable_firewall_fail2ban() {
+    echo "Enabling firewall and Fail2Ban..."
+    sudo ufw enable
+    sudo ufw default deny incoming
+    sudo ufw default allow outgoing
+    sudo systemctl enable --now fail2ban
+}
 
-# Unpin favorite apps from the Ubuntu sidebar
-gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/, 'firefox_firefox.desktop'//" | sed "s/'firefox_firefox.desktop', //")"
-gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/, 'libreoffice-writer.desktop'//" | sed "s/'libreoffice-writer.desktop', //")"
-gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/, 'snap-store_snap-store.desktop'//" | sed "s/'snap-store_snap-store.desktop', //")"
-gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/, 'thunderbird_thunderbird.desktop'//" | sed "s/'thunderbird_thunderbird.desktop', //")"
-gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/, 'yelp.desktop'//" | sed "s/'yelp.desktop', //")"
-gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed "s/, 'org.gnome.Rhythmbox3.desktop'//" | sed "s/'org.gnome.Rhythmbox3.desktop', //")"
-
-# Remove apps I don't need
-sudo snap remove thunderbird firefox
-sudo apt autoremove -y
-
-# Clone your Ubuntu repo
-echo "Cloning configuration repository..."
-git clone https://github.com/ramin-samadi/Ubuntu /tmp/Ubuntu
-git clone https://github.com/orangci/walls-catppuccin-mocha.git ~/Wallpapers
-sudo mv /tmp/Ubuntu/usr/local/bin/change_wallpaper.sh /usr/local/bin/
-
-# Copy configuration files
-echo "Deploying user configurations..."
-sudo mv -f /tmp/Ubuntu/home/.config/* $HOME/.config/
-sudo mv -f /tmp/Ubuntu/home/.vimrc $HOME/
-
-# Enable firewall + Fail2Ban
-sudo ufw enable
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo systemctl enable --now fail2ban
-
-# Add custom configuration to .bashrc
-git clone --depth=1 https://github.com/ChrisTitusTech/mybash.git ~/mybash
-chmod +x ~/mybash/setup.sh
-~/mybash/setup.sh
-git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
-make -C ble.sh install PREFIX=~/.local
-echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
-cat << EOF >> ~/.bashrc
+# Function to add custom configurations to .bashrc
+add_custom_bashrc() {
+    echo "Adding custom configurations to .bashrc..."
+    git clone --depth=1 https://github.com/ChrisTitusTech/mybash.git ~/mybash
+    chmod +x ~/mybash/setup.sh
+    ~/mybash/setup.sh
+    git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
+    make -C ble.sh install PREFIX=~/.local
+    echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
+    cat << EOF >> ~/.bashrc
 alias qdirstat='nohup sudo -E qdirstat'
 export QT_QPA_PLATFORMTHEME=qt5ct
 alias edit='nvim'
@@ -145,46 +160,74 @@ alias serial-number='sudo dmidecode -s system-serial-number'
 alias bios-version='sudo dmidecode -s bios-version'
 alias uefi='sudo systemctl reboot --firmware-setup'
 EOF
+}
 
-# Set catppuccin mocha theme
-## gnome-terminal
-curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v1.0.0/install.py | python3 -
-gsettings set org.gnome.Terminal.ProfilesList default '95894cfd-82f7-430d-af6e-84d168bc34f5'
-gsettings set org.gnome.desktop.interface monospace-font-name 'MesloLGS Nerd Font 12'
-## batcat
-mkdir -p "$(batcat --config-dir)/themes"
-wget -P "$(batcat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Latte.tmTheme
-wget -P "$(batcat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme
-wget -P "$(batcat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Macchiato.tmTheme
-wget -P "$(batcat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
-batcat cache --build
-## Papirus icons
-git clone https://github.com/catppuccin/papirus-folders.git
-cd papirus-folders
-sudo cp -r src/* /usr/share/icons/Papirus
-curl -LO https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-folders/master/papirus-folders && chmod +x ./papirus-folders
-./papirus-folders -C cat-mocha-lavender --theme Papirus-Dark
-gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
-## GTK 3/4 theming
-cd $HOME
-curl -LsSO "https://raw.githubusercontent.com/catppuccin/gtk/v1.0.3/install.py"
-python3 install.py mocha lavender
-gsettings set org.gnome.desktop.interface gtk-theme 'catppuccin-mocha-lavender-standard+default'
+# Function to set the theme
+set_theme() {
+    echo "Setting catppuccin mocha theme..."
+    # gnome-terminal
+    curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v1.0.0/install.py | python3 -
+    gsettings set org.gnome.Terminal.ProfilesList default '95894cfd-82f7-430d-af6e-84d168bc34f5'
+    gsettings set org.gnome.desktop.interface monospace-font-name 'MesloLGS Nerd Font 12'
+    # batcat
+    mkdir -p "$(batcat --config-dir)/themes"
+    wget -P "$(batcat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Latte.tmTheme
+    wget -P "$(batcat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme
+    wget -P "$(batcat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Macchiato.tmTheme
+    wget -P "$(batcat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
+    batcat cache --build
+    # Papirus icons
+    git clone https://github.com/catppuccin/papirus-folders.git
+    cd papirus-folders
+    sudo cp -r src/* /usr/share/icons/Papirus
+    curl -LO https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-folders/master/papirus-folders && chmod +x ./papirus-folders
+    ./papirus-folders -C cat-mocha-lavender --theme Papirus-Dark
+    gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+    # GTK 3/4 theming
+    cd $HOME
+    curl -LsSO "https://raw.githubusercontent.com/catppuccin/gtk/v1.0.3/install.py"
+    python3 install.py mocha lavender
+    gsettings set org.gnome.desktop.interface gtk-theme 'catppuccin-mocha-lavender-standard+default'
+}
 
 # Reload .bashrc
 source ~/.bashrc
 
-# Clean up
-echo "Removing cloned repository..."
-rm -rf /tmp/Ubuntu
+# Function to clean up
+clean_up() {
+    echo "Cleaning up..."
+    rm -rf /tmp/Ubuntu
+    rm /tmp/google-chrome.deb /tmp/teamviewer.deb
+}
 
-echo "Cleaning up downloaded .deb files..."
-rm /tmp/google-chrome.deb /tmp/teamviewer.deb 
+# Function to add user to required groups
+add_user_to_groups() {
+    echo "Adding $USER to required groups..."
+    sudo usermod -aG ubridge,libvirt,kvm,wireshark $(whoami)
+}
 
-# Add user to required groups
-echo "Adding $USER to required groups..."
-sudo usermod -aG ubridge,libvirt,kvm,wireshark $(whoami)
+# Function to reboot system
+reboot_system() {
+    echo "Post-installation setup complete!"
+    sleep 5
+    reboot
+}
 
-echo "Post-installation setup complete!"
-sleep 5
-reboot
+# Main execution flow
+update_system
+add_ppas
+install_apt_packages
+setup_qt5_theme
+install_gns3_iou
+install_deb_packages
+setup_flatpak
+configure_gnome
+remove_unwanted_apps
+clone_repositories
+copy_config_files
+enable_firewall_fail2ban
+add_custom_bashrc
+set_theme
+clean_up
+add_user_to_groups
+reboot_system
