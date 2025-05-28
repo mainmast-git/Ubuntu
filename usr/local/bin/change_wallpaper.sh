aa8974f0adfd30b44d887d79dfca9d241d565fb2
mainmast <ramin-samadi@outlook.com>
2025-05-29 01:22:11 +0200

    Delete usr/local/bin directory

--- usr/local/bin/change_wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get a random wallpaper from /usr/share/backgrounds
-WALLPAPER=$(find $HOME/Wallpapers -type f | shuf -n 1)
-
-# Set the wallpaper using gsettings
-gsettings set org.gnome.desktop.background picture-uri "file://$WALLPAPER"
-gsettings set org.gnome.desktop.background picture-uri-dark "file://$WALLPAPER"
